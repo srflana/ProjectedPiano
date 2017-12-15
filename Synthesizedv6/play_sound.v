ec7593feac9470dd336dc156e088b66008e1aa74
srflana <srflana@mit.edu>
2017-12-15 08:58:22 -0500

    Update play_sound.v

--- Synthesizedv6/play_sound.v
+++ Synthesizedv6/play_sound.v
@@ -19,34 +19,34 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module play_sound(
-    input [16:0] key_num,
-	 input clock,
-	 input [7:0] to_ac97_data2,
-	 input [1:0] state,
-	 input shift,
-    output reg [7:0] to_ac97_data
+   input [16:0] key_num,
+   input clock,
+   input [7:0] to_ac97_data2,
+   input [1:0] state,
+   input shift,
+   output reg [7:0] to_ac97_data
     );
-	 wire new_ready;
-	 wire [19:0] tone;
-	 wire [19:0] tone2;
-	 reg restart;
-	 reg [16:0] old_key_num;
+    wire new_ready;
+    wire [19:0] tone;
+    wire [19:0] tone2;
+    reg restart;
+    reg [16:0] old_key_num;
 	 
-	 ready_generator generator(.clk(clock),.restart(restart),.shift(shift),.key_num(key_num),.ready(new_ready));
+    ready_generator generator(.clk(clock),.restart(restart),.shift(shift),.key_num(key_num),.ready(new_ready));
 	 //ready_generator generator2(.clk(clock),.restart(restart),.key_num(17'b0_0000_0000_0000_1000),.ready(new_ready2));
     tone750hz xxx(.clock(clock),.ready(new_ready),.restart(restart),.pcm_data(tone));
 	 //tone750hz xxx2(.clock(clock),.ready(new_ready2),.restart(restart),.pcm_data(tone2));
 	 
-	 always @(posedge clock) begin
-	    if (key_num[16:0] != old_key_num[16:0]) restart <= 1;
-		 else restart <= 0;
-		 old_key_num <= key_num[16:0];
-		 
-		 if (state == 3) to_ac97_data[7:0] <= to_ac97_data2;
-		 else if (key_num[16:0] == 0) to_ac97_data[7:0] <= 8'b0;
-		 else to_ac97_data[7:0] <= tone[19:12];
+    always @(posedge clock) begin
+       if (key_num[16:0] != old_key_num[16:0]) restart <= 1;
+       else restart <= 0;
+       old_key_num <= key_num[16:0];
+
+       if (state == 3) to_ac97_data[7:0] <= to_ac97_data2;
+       else if (key_num[16:0] == 0) to_ac97_data[7:0] <= 8'b0;
+       else to_ac97_data[7:0] <= tone[19:12];
 		 //else to_ac97_data[7:0] <= {0,tone[19:13]}+{0,tone2[19:13]};
-	 end
+    end
 	 
 	 //assign restart = (key_num[16:0] != old_key_num[16:0] ) ? 1:0;
 	 
